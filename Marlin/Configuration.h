73100c65c708d21a93b8bd823e0ef988cc9f9b1f
Merlin Webster <mjftwebster@gmail.com>
2020-05-31 19:53:53 +0100

    Prevent probing outside of bed during auto level

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -929,10 +929,10 @@
 #define NOZZLE_TO_PROBE_OFFSET { 0, 37, 0 }
 
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 10
+#define MIN_PROBE_EDGE 30
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 8000
+#define XY_PROBE_SPEED 10000
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
