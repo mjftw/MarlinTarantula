a71c0c517212a131421c7a9b47268e86beedbd4b
Merlin Webster <mjftwebster@gmail.com>
2020-07-10 09:15:06 +0100

    Calibrate steps/mm for Hemera extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -723,7 +723,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 409 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 395.5 }
 
 /**
  * Default Max Feed Rate (mm/s)
